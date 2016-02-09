b0433df921464e2996814f3d28ec5d0fb1fccde3
deibytb <deiby@devacademy.la>
2016-02-09 13:00:45 -0500

    Initial Commit

---
+++ bluetooth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bluetooth
+//
+//  Created by Deiby Toralva Baldeón on 9/02/16.
+//  Copyright © 2016 devAcademy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
